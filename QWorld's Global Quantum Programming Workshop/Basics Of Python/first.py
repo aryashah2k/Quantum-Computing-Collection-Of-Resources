0b9056f7b5d306dddfd300668be617afcce2b536
Arya Shah <arya.shah82@nmims.edu.in>
2020-11-21 12:58:34 +0530

    Add Basics Of Python Notebooks + Readme

---
+++ QWorld's Global Quantum Programming Workshop/Basics Of Python/first.py
@@ -0,0 +1,7 @@
+
+print("hello world")
+
+str="*"
+for i in range(5):
+    print(str)
+    str+="*"  
---
+++ QWorld's Global Quantum Programming Workshop/Basics Of Python/qlatvia.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.insert(0, '../include')
+
+from drawing import draw_axes, draw_unit_circle, draw_quantum_state, draw_qubit
+
+from quantum import random_quantum_state, random_quantum_state2, angle_quantum_state
